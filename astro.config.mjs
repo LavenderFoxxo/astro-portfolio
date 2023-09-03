2b0109414810afde0c24e89c61c90f64d37680f3
Alexander Hyman <alexander.hyman05@gmail.com>
2023-09-03 14:03:53 -0700

    feat: init

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  integrations: [tailwind()],
+  redirects: {
+    '/twitter': 'https://x.com/alexanderhy04',
+    '/github': 'https://github.com/LavenderFoxxo'
+  }
+});
